18b2c7bbf0681fcba46f7bdf7505e229623e8424
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2026-02-11 13:06:24 +0000

    aula2

---
+++ helloworld.js
@@ -0,0 +1,20 @@
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 9;
+let nota4 = 6;
+
+const nomeAluno = "Mei Kilo";
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+if(media >= 7){
+    console.log("O aluno " + nomeAluno + " está aprovado.")
+} if(media >= 5 && media < 7){
+    console.log("O aluno está de se liga.")
+}
+if(media < 5){
+    console.log("O aluno está reprovado.")
+}
+
+console.log("O aluno: " + nomeAluno);
+console.log("Tem a média:" + media);
---
+++ tipoArquivo.js
@@ -0,0 +1,9 @@
+testString = "teste"
+testInt = 50
+testeFloat = 50.0
+testeBolean = true 
+
+console.log(typeof(testString));
+console.log(typeof(testInt));
+console.log(typeof(testeFloat));
+console.log(typeof(testeBolean));
