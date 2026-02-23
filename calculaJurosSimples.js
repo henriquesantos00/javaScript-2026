d564552df63a8cfb84042448dc83c21d21108c1d
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2026-02-23 13:02:25 +0000

    final

---
+++ calculaJurosSimples.js
@@ -0,0 +1,12 @@
+function calculaJurosSimples(capital,taxa, tempo){
+    let juros = capital * (taxa/100 ) * tempo; 
+    let montante = capital + juros;
+
+    return montante;
+}
+
+function calculaJurosCompostos (capital, taxa, qtd_de_juros)
+{ let montante2 = capital * (1 + taxa) ** qtd_de_juros;
+    return montante2; } 
+     let conta2 = calculaJurosCompostos(100, 1, 10);
+      console.log("O montante é: " + conta2);
