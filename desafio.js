478534cd34995aa9b22b6ab50f0a6b9535e2cdbc
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2026-03-09 17:11:15 +0000

    desafio

---
+++ desafio.js
@@ -0,0 +1,17 @@
+let nome = "João kilo";  
+let idade = 25;     
+let temCupom = true; 
+
+const PRECO_CHEIO = 30;
+
+let precoFinal;
+
+if (idade >= 60) {
+    precoFinal = PRECO_CHEIO / 2; 
+} else if (idade < 18 || temCupom) {
+    precoFinal = PRECO_CHEIO * 0.8; 
+} else {
+    precoFinal = PRECO_CHEIO; 
+}
+
+console.log(`Olá, ${nome}! O valor do seu ingresso é R$ ${precoFinal.toFixed(2)}`);
